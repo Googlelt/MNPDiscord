e95d1a807746416acbc2f1b4402ea9f7b8d38670
Kaan Yucesoy <kaanyt123@gmail.com>
2019-06-01 22:48:46 +0200

    test

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -98,7 +98,7 @@ module.exports.run = async (client, msg, args) => {
         } else if (args[0] === "pull") {
             // exec(`cd ../ && git pull ${client.config.git}`, async () => {
             exec(`git pull ${client.config.git}`, async () => {
-                await msg.reply("Rebooting");
+                await msg.reply("Rebooting.");
 
                 process.exit();
             });
