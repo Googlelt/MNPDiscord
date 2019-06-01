24bc4f2f9d2b59cd0014961f9add6c1b9d15b657
Kaan Yucesoy <kaanyt123@gmail.com>
2019-06-01 22:35:22 +0200

    Fix pull hopefully

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -96,7 +96,8 @@ module.exports.run = async (client, msg, args) => {
 
             msg.channel.send("```" + evaled + "```");
         } else if (args[0] === "pull") {
-            exec(`cd ../ && git pull ${client.config.git}`, async () => {
+            // exec(`cd ../ && git pull ${client.config.git}`, async () => {
+            exec(`git fetch--all & git reset--hard origin / master & git pull ${client.config.git}`, async () => {
                 await msg.reply("Rebooting");
 
                 process.exit();
