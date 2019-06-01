b8783dc64400186af21e9c1670791e0b9addf3fd
Kaan Yucesoy <kaanyt123@gmail.com>
2019-06-01 22:47:14 +0200

    reverted smth

--- src/commands/admin.js
+++ src/commands/admin.js
@@ -97,7 +97,7 @@ module.exports.run = async (client, msg, args) => {
             msg.channel.send("```" + evaled + "```");
         } else if (args[0] === "pull") {
             // exec(`cd ../ && git pull ${client.config.git}`, async () => {
-            exec(`git fetch--all & git reset--hard origin / master & git pull ${client.config.git}`, async () => {
+            exec(`git pull ${client.config.git}`, async () => {
                 await msg.reply("Rebooting");
 
                 process.exit();
